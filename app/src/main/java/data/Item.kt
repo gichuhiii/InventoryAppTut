935438cbe33ea9364315898ffa956733058d444c
gichuhiii <gnwcocoe@gmail.com>
2023-03-15 20:24:04 +0300

    db

--- app/src/main/java/data/Item.kt
+++ app/src/main/java/data/Item.kt
@@ -3,6 +3,8 @@ package data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.coroutines.launch
+import androidx.lifecycle.viewModelScope
 
 @Entity(tableName = "item")
 
